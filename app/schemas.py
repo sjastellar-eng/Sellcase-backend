efd005e1eea31462d079a2d530840f78c66baa47
sjastellar-eng <sja.stellar@gmail.com>
2025-12-02 00:04:01 +0200

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -31,6 +31,9 @@ class UserBase(BaseModel):
 class UserCreate(UserBase):
     password: str
 
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
 
 class UserOut(UserBase):
     id: int
